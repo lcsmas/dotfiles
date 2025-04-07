8cadd6c07a044148d92d620abfd31c2c69c8d23c
Lucas Mas <lucas_mas@msn.com>
2025-04-07 12:03:21 +0200

    add tmux-sessionizer and tmux-tpm

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -10,15 +10,18 @@ sudo dnf install -y wayland sway pipewire wireplumber tmux git wget curl gcc mak
 dnf copr enable -y pgdev/ghostty
 dnf install ghostty
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 git clone https://www.github.com/lcsmas/dotfiles
 cd dotfiles
-DOTFILES=$(ls -la | grep -v '^d' | awk '{ print $9 }' | grep -v '^$' | grep '^\.' | grep -v 'example$')
+DOTFILES=$(ls -la | grep -v '^d' | awk '{ print $9 }' | grep -v '^$' | grep '^\.' | grep -v 'example$' | grep -v '.gitignore' | grep -v '.bashrc')
 DOTFILE_DIR=$(pwd)
 
 for file in $DOTFILES; do
 		ln -s $DOTFILE_DIR/$file ~/$file
 		echo "Created symlink for $file"
 done
+ln -s $DOTFILE_DIR/tmux-sessionizer.sh ~/.local/bin/tmux-sessionizer.sh
 
 echo "Sudo password is required to change the default shell to zsh."
 sudo chsh -s $(which zsh) $USER
