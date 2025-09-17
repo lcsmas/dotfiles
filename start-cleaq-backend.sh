fea68533fd89bf0c470b3f6cc7da3ce8f6c4465f
Lucas Mas <lucas.mas@mobile.club>
2025-09-17 09:58:31 +0200

    tmux start cleaq wait for mongo

--- start-cleaq-backend.sh
+++ start-cleaq-backend.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-tmux neww -c ~/dev/cleaq/backend-dev-env/ "docker compose up"
-tmux splitw -c ~/dev/cleaq/backend-api/ -v "./gradlew :management:bootRun --args='--spring.profiles.active=local,custom'; zsh -i"
-tmux splitw -c ~/dev/cleaq/backend-api/ -h "./gradlew :order:bootRun --args='--spring.profiles.active=local,custom'; zsh -i"
+if tmux list-windows -F "#W" | grep -q "api-run"; then
+  tmux kill-window -t api-run
+fi
+
+tmux neww -n api-run -c ~/dev/cleaq/backend-dev-env/ "docker compose up"
+
+echo -n "Waiting for mongo1 to accept connection."
+until docker exec -t mongo1 mongosh --quiet --eval "db.serverStatus()" > /dev/null 2>&1; do
+    sleep 1; echo -n "." 
+done
+echo
+
+tmux splitw -c ~/dev/cleaq/backend-api/ -v "./gradlew :management:bootRun --args='--spring.profiles.active=local,custom' --continuous; zsh -i"
+tmux splitw -c ~/dev/cleaq/backend-api/ -h "./gradlew :order:bootRun --args='--spring.profiles.active=local,custom' --continuous; zsh -i"
